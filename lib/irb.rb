802b155d46fdfd09b121317454e7be311687ed73
kazu <kazu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2018-11-12 02:00:08 +0000

    lib/irb.rb: fix up r65674
    git-svn-id: svn+ssh://ci.ruby-lang.org/ruby/trunk@65676 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- lib/irb.rb
+++ lib/irb.rb
@@ -743,7 +743,7 @@ class Binding
   #       def initialize
   #         @cooked = false
   #         binding.irb
-  #         puts @cooked
+  #         puts "Cooked potato: #{@cooked}"
   #       end
   #     end
   #
@@ -754,7 +754,7 @@ class Binding
   #
   #     $ ruby potato.rb
   #
-  #     From: potato.rb @ line 3 :
+  #     From: potato.rb @ line 4 :
   #
   #         1: class Potato
   #         2:   def initialize
@@ -785,7 +785,7 @@ class Binding
   # output printed to standard output in this example:
   #
   #     irb(#<Potato:0x00007feea1916670>):005:0> exit
-  #     $ Cooked potato: true
+  #     Cooked potato: true
   #
   #
   # See IRB@IRB+Usage for more information.
