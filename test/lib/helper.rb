4d61ab2024fbec7bcab021203463a2546c517844
st0012 <stan001212@gmail.com>
2022-10-24 23:08:32 +0100

    Extract without_rdoc helper

--- test/lib/helper.rb
+++ test/lib/helper.rb
@@ -9,6 +9,19 @@ module Test
       def windows? platform = RUBY_PLATFORM
         /mswin|mingw/ =~ platform
       end
+
+      def without_rdoc(&block)
+        ::Kernel.send(:alias_method, :old_require, :require)
+
+        ::Kernel.define_method(:require) do |name|
+          raise LoadError, "cannot load such file -- rdoc (test)" if name.match?("rdoc") || name.match?(/^rdoc\/.*/)
+          ::Kernel.send(:old_require, name)
+        end
+
+        yield
+      ensure
+        ::Kernel.send(:alias_method, :require, :old_require)
+      end
     end
   end
 end
