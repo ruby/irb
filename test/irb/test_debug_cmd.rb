6f3c3c717aa5ddb8e6fa5245e26945c5b2f6fdf7
tompng <tomoyapenguin@gmail.com>
2023-05-17 02:15:15 +0900

    Force use Reline::GeneralIO as Reline::IOGate and remove RUBY_DEBUG_NO_RELINE option for debug test

--- test/irb/test_debug_cmd.rb
+++ test/irb/test_debug_cmd.rb
@@ -17,7 +17,7 @@ module TestIRB
 
   class DebugCommandTestCase < TestCase
     IRB_AND_DEBUGGER_OPTIONS = {
-      "RUBY_DEBUG_NO_RELINE" => "true", "NO_COLOR" => "true", "RUBY_DEBUG_HISTORY_FILE" => ''
+      "NO_COLOR" => "true", "RUBY_DEBUG_HISTORY_FILE" => ''
     }
 
     def setup
@@ -213,12 +213,7 @@ module TestIRB
       rc_file = File.open(File.join(tmp_dir, ".irbrc"), "w+")
       rc_file.write(<<~RUBY)
         IRB.conf[:USE_SINGLELINE] = true
-        require 'readline'
-        if Readline.const_defined?(:IOGate)
-          def (Reline::IOGate).cursor_pos
-            Reline::GeneralIO.cursor_pos
-          end
-        end
+        Reline.const_set(:IOGate, Reline::GeneralIO)
       RUBY
       rc_file.close
 
