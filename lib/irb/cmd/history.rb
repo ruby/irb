e8d0fbf3cb84509f9010969f5422c601cd8f9a40
Gary Tou <gary@garytou.com>
2023-11-13 15:23:00 -0800

    Implement `history` command
    Lists IRB input history with indices. Also aliased as `hist`.

---
+++ lib/irb/cmd/history.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "stringio"
+require_relative "nop"
+require_relative "../pager"
+
+module IRB
+  # :stopdoc:
+
+  module ExtendCommand
+    class History < Nop
+      category "IRB"
+      description "Show the input history."
+
+      def execute(*)
+        output = StringIO.new
+        irb_context.io.class::HISTORY.each_with_index.reverse_each do |input, index|
+          header = "#{index}: "
+          header_length = header.size
+
+          lines = input.split("\n")
+          first_line = header + lines[0]
+
+          truncated_input = lines[1..1]
+          truncated_input << "..." if lines.size > 2
+
+          truncated_input = truncated_input.map do |line|
+            " " * header_length + line
+          end
+
+          output.puts [first_line, *truncated_input].join("\n")
+        end
+
+        Pager.page_content(output.string)
+      end
+    end
+  end
+
+  # :startdoc:
+end
