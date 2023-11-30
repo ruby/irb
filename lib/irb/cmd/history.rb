648d86d4852b55562ed93770775289cc5f8a7626
Gary Tou <gary@garytou.com>
2023-11-29 16:19:23 -0800

    Refactor line truncation

--- lib/irb/cmd/history.rb
+++ lib/irb/cmd/history.rb
@@ -16,19 +16,18 @@ module IRB
         output = StringIO.new
         irb_context.io.class::HISTORY.each_with_index.reverse_each do |input, index|
           header = "#{index}: "
-          header_length = header.size
 
-          lines = input.split("\n") || [""]
-          first_line = header + lines[0]
+          first_line, *other_lines = input.split("\n") || [""]
+          first_line.prepend header
 
-          truncated_input = lines[1..1]
-          truncated_input << "..." if lines.size > 2
+          truncated_lines = other_lines.slice!(1..) # Show 1 additional line (2 total)
+          other_lines << "..." if truncated_lines&.any?
 
-          truncated_input = truncated_input.map do |line|
-            " " * header_length + line
+          other_lines.map! do |line|
+            " " * header.length + line
           end
 
-          output.puts first_line, *truncated_input
+          output.puts first_line, *other_lines
         end
 
         Pager.page_content(output.string)
