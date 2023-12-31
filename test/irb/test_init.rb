cafffcb61173c38de5d926940958b2e02818a464
Sorah Fukumori <her@sorah.jp>
2024-01-01 06:14:38 +0900

    test_recovery_sigint: Ensure precondition is met
    test_recovery_sigint depends on its process has SIG_DEF sigaction for
    SIGINT. When its parent process set SIG_IGN which inherits to children,
    then this test fails. This patch ensures this precondition met
    regardless of inherited sigaction from its parent.

--- test/irb/test_init.rb
+++ test/irb/test_init.rb
@@ -69,9 +69,11 @@ module TestIRB
     def test_recovery_sigint
       pend "This test gets stuck on Solaris for unknown reason; contribution is welcome" if RUBY_PLATFORM =~ /solaris/
       bundle_exec = ENV.key?('BUNDLE_GEMFILE') ? ['-rbundler/setup'] : []
-      status = assert_in_out_err(bundle_exec + %w[-W0 -rirb -e binding.irb;loop{Process.kill("SIGINT",$$)} -- -f --], "exit\n", //, //)
+      # IRB should restore SIGINT handler
+      status = assert_in_out_err(bundle_exec + %w[-W0 -rirb -e Signal.trap("SIGINT","DEFAULT");binding.irb;loop{Process.kill("SIGINT",$$)} -- -f --], "exit\n", //, //)
       Process.kill("SIGKILL", status.pid) if !status.exited? && !status.stopped? && !status.signaled?
     end
+
 
     def test_no_color_environment_variable
       orig_no_color = ENV['NO_COLOR']
