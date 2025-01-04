b72501cea4d792a144795b5f8745736c26dff120
Stan Lo <stan001212@gmail.com>
2025-01-04 21:22:41 +0000

    Streamline command parsing and introduce warnings for incorrect command aliases

--- test/irb/command/test_command_aliasing.rb
+++ test/irb/command/test_command_aliasing.rb
@@ -24,16 +24,26 @@ module TestIRB
         type "c"
         type "exit"
       end
-      assert_include(out, "Using command alias for helper method 'conf' is not supported")
+      assert_include(out, "Using command alias `c` for helper method `conf` is not supported.")
       assert_not_include(out, "Maybe IRB bug!")
     end
 
-    def test_incorrect_alias_triggers_warning
+    def test_alias_to_non_existent_command_triggers_warning
+      message = "You're trying to use command alias `f` for command `foo`, but `foo` does not exist."
       out = run_ruby_file do
         type "f"
         type "exit"
       end
-      assert_include(out, "Command 'foo' does not exist")
+      assert_include(out, message)
+      assert_not_include(out, "Maybe IRB bug!")
+
+      # Local variables take precedence over command aliases
+      out = run_ruby_file do
+        type "f = 123"
+        type "f"
+        type "exit"
+      end
+      assert_not_include(out, message)
       assert_not_include(out, "Maybe IRB bug!")
     end
   end
