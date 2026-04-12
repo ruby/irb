2b0188284aaf005d925e6db97217d09855bad474
ahogappa <ahogappa@gmail.com>
2026-04-12 11:47:41 +0000

    refactor: stop modifying Ruby::Box#require and use box.require for file detection

--- lib/irb/reloadable_require.rb
+++ lib/irb/reloadable_require.rb
@@ -9,53 +9,18 @@ require 'set'
 module IRB
   # Provides reload-aware require functionality for IRB.
   #
+  # This feature is experimental and requires Ruby::Box (Ruby 4.0+).
+  #
   # Limitations:
   # - Native extensions cannot be reloaded (load doesn't support them)
-  # - Files loaded via box.require are not tracked
   # - Constant redefinition warnings will appear on reload (uses load internally)
-  # - Context mode 5 (running IRB inside a Ruby::Box) is not supported
-  #
-  # This feature requires Ruby::Box (Ruby 4.0+).
-
-  unless Ruby::Box.method_defined?(:__irb_original_require__)
-    Ruby::Box.class_eval do
-      alias_method :__irb_original_require__, :require
-      alias_method :__irb_original_require_relative__, :require_relative
-    end
-  end
-
-  Ruby::Box.class_eval do
-    def __irb_reloadable_require__(feature)
-      unless IRB::ReloadableRequire.autoload_files.include?(feature)
-        return __irb_original_require__(feature)
-      end
-
-      IRB::ReloadableRequire.autoload_files.delete(feature)
-      IRB::ReloadableRequire.track_and_load_files($LOADED_FEATURES, Ruby::Box.main) { __irb_original_require__(feature) }
-    end
-
-    def __irb_reloadable_require_relative__(feature)
-      __irb_original_require_relative__(feature)
-    end
-  end
 
   module ReloadableRequire
     @reloadable_files = Set.new
-    @autoload_files = Set.new
+    @autoload_features = {}
 
     class << self
-      attr_reader :reloadable_files, :autoload_files
-
-      def extended(base)
-        apply_autoload_hook
-      end
-
-      def apply_autoload_hook
-        Ruby::Box.class_eval do
-          alias_method :require, :__irb_reloadable_require__
-          alias_method :require_relative, :__irb_reloadable_require_relative__
-        end
-      end
+      attr_reader :reloadable_files, :autoload_features
 
       def track_and_load_files(source, current_box)
         before = source.dup
@@ -76,6 +41,15 @@ module IRB
 
         result
       end
+
+      def collect_autoloaded_files
+        @autoload_features.each_value do |feature|
+          resolved = $LOAD_PATH.resolve_feature_path(feature) rescue nil
+          next unless resolved && resolved.first == :rb
+          path = resolved[1]
+          @reloadable_files << path if $LOADED_FEATURES.include?(path)
+        end
+      end
     end
 
     private
@@ -85,18 +59,16 @@ module IRB
       box.eval("$LOAD_PATH.concat(#{caller_box.eval('$LOAD_PATH')})")
       box.eval("$LOADED_FEATURES.concat(#{caller_box.eval('$LOADED_FEATURES')})")
 
-      ReloadableRequire.track_and_load_files(box.eval('$LOADED_FEATURES'), caller_box) { box.__irb_original_require__(feature) }
+      ReloadableRequire.track_and_load_files(box.eval('$LOADED_FEATURES'), caller_box) { box.require(feature) }
     end
 
     def require(feature)
       caller_loc = caller_locations(1, 1).first
-      current_box = Ruby::Box.main
-      return current_box.__irb_original_require__(feature) unless caller_loc.path.end_with?("(irb)")
+      return super unless caller_loc.path.end_with?("(irb)")
 
-      resolved = current_box.eval("$LOAD_PATH.resolve_feature_path(#{feature.dump})")
-      return current_box.__irb_original_require__(feature) unless resolved&.first == :rb
-
-      reloadable_require_internal(feature, current_box)
+      reloadable_require_internal(feature, Ruby::Box.main)
+    rescue LoadError
+      super
     end
 
     def require_relative(feature)
@@ -109,10 +81,12 @@ module IRB
       end
 
       reloadable_require_internal(File.expand_path(feature, Dir.pwd), current_box)
+    rescue LoadError
+      super
     end
 
     def autoload(const, feature)
-      ReloadableRequire.autoload_files << feature
+      ReloadableRequire.autoload_features[const.to_s] = feature
       Ruby::Box.main.eval("Kernel.autoload(:#{const}, #{feature.dump})")
     end
   end
